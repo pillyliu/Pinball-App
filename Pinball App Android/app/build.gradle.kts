18c9e687fb78f60a31ce2e1b62590b68e667400d
Peter <pillyliu@gmail.com>
2026-02-26 08:11:27 -0500

    Sync pruned v3 starter packs

--- Pinball App Android/app/build.gradle.kts
+++ Pinball App Android/app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.pillyliu.pinballandroid"
         minSdk = 26
         targetSdk = 36
-        versionCode = 26
-        versionName = "2.1.5"
+        versionCode = 27
+        versionName = "2.1.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
