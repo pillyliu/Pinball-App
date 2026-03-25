608ee0e3d015b0efd05502fbbabfe01bd6dc6e22
Peter <pillyliu@gmail.com>
2026-03-25 14:19:36 -0400

    Bump Android support dependencies

--- Pinball App Android/app/build.gradle.kts
+++ Pinball App Android/app/build.gradle.kts
@@ -92,9 +92,9 @@ dependencies {
     val bom = platform("androidx.compose:compose-bom:2025.01.00")
     val cameraXVersion = "1.5.3"
 
-    implementation("androidx.core:core-ktx:1.17.0")
+    implementation("androidx.core:core-ktx:1.18.0")
     implementation("androidx.core:core-splashscreen:1.2.0")
-    implementation("androidx.activity:activity-compose:1.12.4")
+    implementation("androidx.activity:activity-compose:1.13.0")
     implementation("androidx.recyclerview:recyclerview:1.4.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.10.0")
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.10.0")
@@ -123,7 +123,7 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 
     testImplementation("junit:junit:4.13.2")
-    testImplementation("org.robolectric:robolectric:4.16")
+    testImplementation("org.robolectric:robolectric:4.16.1")
     testImplementation("org.json:json:20251224")
     androidTestImplementation("androidx.test.ext:junit:1.3.0")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.7.0")
